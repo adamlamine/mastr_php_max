159b9e20da6cc3c1e9004973d448841d39778bad
Adam Lamine <adam.lamine@e-2.at>
2020-04-19 22:31:27 +0200

    full low latency playback

--- icecast_test.php
+++ icecast_test.php
@@ -23,6 +23,10 @@ var sourceBuffer;
 
 mediaSource.addEventListener('sourceopen', function() {
   sourceBuffer = mediaSource.addSourceBuffer('audio/mpeg');
+  
+  setTimeout(function (){
+      audio.play()
+  }, 500);
 });
 
 audio.src = URL.createObjectURL(mediaSource);
@@ -32,15 +36,8 @@ function play(url) {
   fetch(url).then(function(response) {
     var reader = response.body.getReader();
     function read() {
-      return reader.read().then(({ value, done })=> {
-        context.decodeAudioData(value.buffer, function(buffer) {
-          audioStack.push(buffer);
-          if (audioStack.length) {
-              scheduleBuffers();
-          }
-        }, function(err) {
-          console.log("err(decodeAudioData): "+err);
-        });
+      reader.read().then(({ value, done })=> {
+        sourceBuffer.appendBuffer(value);
         read();
       });
     }
@@ -48,20 +45,6 @@ function play(url) {
   })
 }
 
-  function scheduleBuffers() {
-      while ( audioStack.length && sourceBuffer) {
-          var buffer    = audioStack.shift();
-          var source    = context.createBufferSource();
-          console.log(buffer);
-          sourceBuffer.appendBuffer(buffer);
-          //source.buffer = buffer;
-          //source.connect(context.destination);
-          //if (nextTime == 0)
-          //    nextTime = context.currentTime;  /// add 50ms latency to work well across systems - tune this if you like
-          //source.start(nextTime);
-          //nextTime += source.buffer.duration; // Make the next buffer wait the length of the last buffer before being played
-      };
-  }
 
 </script>
 
